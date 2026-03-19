8a0691c060046440d0f6179073bc89738491841b
JordanGethers <jordansgethers@protonmail.com>
2026-03-19 07:13:15 -0400

    Read and printed out authlog file

--- log_parser.py
+++ log_parser.py
@@ -1,2 +1,20 @@
 #Log Parser Created (3/19)
-#Commit 0 - Jordan Gethers - Initial Plan/Commit
+#Commit 0 - Jordan Gethers - Initial Plan/Commit    
+
+#TODO: Use the re module to extract failed SSH login lines
+#TODO: Learn regex: re.search(), re.findall(), named groups
+
+# Later TODO: Group failures by IP using a dict. COunt and sort Descending.
+#             Output to CSV with a timestamp column using the csv module
+
+#Imports
+import re, os
+
+#print(os.getcwd())
+script_dir = os.path.dirname(__file__)
+#print(script_dir)
+
+file_path = os.path.join(script_dir, "logFiles/sample_auth.log")
+with open(file_path, 'r') as f: #Add encoding?
+    log_txt = f.read()
+    print(log_txt)
